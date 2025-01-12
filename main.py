768895a9defd3d031c2bd909b8fd2c6b3a38d989
obagaev <obagaev11@gmail.com>
2025-01-12 11:20:43 -0600

    Added Clock feature

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from constants import *
 def main():
     
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -19,6 +21,7 @@ def main():
             
         screen.fill((0,0,0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
