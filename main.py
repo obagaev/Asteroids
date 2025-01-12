a9e35162163e41be17af3c9c182c6b5e1d2a6fee
obagaev <obagaev11@gmail.com>
2025-01-12 12:50:56 -0600

    Draw a player on the screen

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from player import Player
 from constants import *
+
 def main():
     
     pygame.init()
@@ -12,6 +14,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -19,8 +23,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
-        screen.fill((0,0,0))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 
